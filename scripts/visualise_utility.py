acb6f2c14d758ec8aa75d2ec81ae7f2cd6acd4ca
Peter Warrington <sothisispeter@gmail.com>
2025-12-26 17:16:25 +0000

    ms since start time display for visualiser

--- scripts/visualise_utility.py
+++ scripts/visualise_utility.py
@@ -4,8 +4,11 @@ import json
 import argparse
 from datetime import datetime
 
-def time(e):
-    return datetime.fromtimestamp(float(e["time"]) / 1000.0)
+def to_ms(t):
+    return float(t) / 1000.0
+
+def tz_time(e):
+    return datetime.fromtimestamp(to_ms(e["time"]))
 
 arg_parser = argparse.ArgumentParser(
                     prog='visualise_utility.py',
@@ -13,19 +16,34 @@ arg_parser = argparse.ArgumentParser(
                     epilog='TCPLog Visualisation Utility (C) Peter Warrington 2026')
 
 arg_parser.add_argument("input", type=str)
+arg_parser.add_argument("-t", "--timestamp-display", help="Display time as timestamp rather than ms since start.", action='store_true')
 args = arg_parser.parse_args()
 
 f = open(args.input)
 data = json.loads(f.read())
+
+start_time = to_ms(data[0]["time"])
+def ms_from_start(e):
+    return to_ms(e["time"]) - start_time
+
+time = ms_from_start
+if args.timestamp_display:
+    time = tz_time
 
 fig, ax1 = plt.subplots()
 
 cwnd_points = np.array([(time(e), e["data"]["state_variables"]["cwnd"]) for e in data])
 ax1.plot(cwnd_points[:,0], cwnd_points[:,1], marker = 'o', color="#00F")
 
-ax1.set_xlabel("Time")
+if not args.timestamp_display:
+    ax1.set_xlabel("Time (ms since start)")
+else:
+    ax1.set_xlabel("Time")
+
 ax1.set_ylabel("cwnd")
 ax1.yaxis.label.set_color("#00F")
+ax1.set_xlim(0, ax1.get_xlim()[1])
+ax1.set_ylim(0, ax1.get_ylim()[1])
 
 ax2 = ax1.twinx()
 
