eb74b0e6918b52b401adfd5d5b86d07f0722d5af
Peter Warrington <sothisispeter@gmail.com>
2026-02-02 19:03:00 +0000

    begin dumbell topology

--- scripts/mininet_tester.py
+++ scripts/mininet_tester.py
@@ -11,21 +11,24 @@ import time
 import os
 import signal
 from sys import stdout
+import subprocess
 
 arg_parser = argparse.ArgumentParser(
                     prog='mininet_tester.py',
                     description='Collect test TCPLog data using Mininet.',
                     epilog='TCPLog Mininet Testing Utility (C) Peter Warrington 2026')
-arg_parser.add_argument("-b", "--bandwidth", type=float, help="Bandwidth of host link.", default=1000)
-arg_parser.add_argument("-d", "--delay", type=float, help="Delay in milliseconds.", default=5)
-arg_parser.add_argument("-l", "--loss", type=float, help="Loss as percentage.", default=0.2)
+arg_parser.add_argument("-b", "--bandwidth", type=float, help="Bandwidth of host link.", default=100)
+arg_parser.add_argument("-d", "--delay", type=float, help="Delay in milliseconds.", default=50)
+arg_parser.add_argument("-l", "--loss", type=float, help="Loss as percentage.", default=0.1)
 arg_parser.add_argument("-o", "--output", type=str, help="File to write test log to.", required=True)
 arg_parser.add_argument("-t", "--duration", type=float, help="Length of time in which to test in seconds.", default=10)
 arg_parser.add_argument("-s", "--size", type=int, help="Maximum size of upload (mb).", default=512)
-arg_parser.add_argument("-q", "--queue-size", type=int, help="Max queue size of switch.", default=1e9)
+arg_parser.add_argument("-q", "--queue-size", type=int, help="Max queue size of switch.", default=100)
 arg_parser.add_argument("-p", "--pcap", action="store_true", help="Capture pcap as well as tcplog.")
 
 args = arg_parser.parse_args()
+
+host_ids = []
 
 class SingleSwitchTopo( Topo ):
     "Single switch connected to k hosts."
@@ -33,27 +36,31 @@ class SingleSwitchTopo( Topo ):
     def build( self, k=2, **_opts):
         "k: number of hosts"
         self.k = k
-        switch = self.addSwitch( 's1' )
-        for h in range(1, k+1 ):
-            host = self.addHost( 'h%s' % h )
-            #  https://mininet.org/api/classmininet_1_1topo_1_1Topo.html
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch('s2')
+        self.addLink(switch1, switch2,  bw=args.bandwidth, delay=f"{args.delay}ms", loss=args.loss, max_queue_size=args.queue_size)
+        for h in range(1, k+1):
+            host_id = 'h%s' % h
+            host_ids.append(host_id)
+            host = self.addHost(host_id)
+            switch = switch1 if h < (k+1) / 2 else switch2
             self.addLink( host, switch, bw=args.bandwidth, delay=f"{args.delay}ms", loss=args.loss, max_queue_size=args.queue_size)
 
 
 def main():
     topo = SingleSwitchTopo()
 
-    net = Mininet(topo=topo, link=TCLink)
+    net = Mininet(topo=topo, link=TCLink, cleanup=True)
     net.start()
 
     h1, h2 = net.get('h1', 'h2')
 
     tempdir = tempfile.gettempdir()
 
-    make_proc = h1.popen(["make", "install"], stdout=stdout, stderr=stdout)
+    make_proc = subprocess.Popen(["make", "install"], stdout=stdout, stderr=stdout)
     make_proc.wait()
 
-    dd_proc = h1.popen(["dd", "if=/dev/urandom", f"of={tempdir}/random", "bs=1M", f"count={args.size}", "iflag=fullblock"], stdout=stdout, stderr=stdout)
+    dd_proc = subprocess.Popen(["dd", "if=/dev/urandom", f"of={tempdir}/random", "bs=1M", f"count={args.size}", "iflag=fullblock"], stdout=stdout, stderr=stdout)
     dd_proc.wait()
 
     server_proc = h1.popen(["python3", "-m", "http.server", "4444", "-d", tempdir], stdout=stdout, stderr=stdout)
